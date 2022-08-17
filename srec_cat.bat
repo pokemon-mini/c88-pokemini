ff4cb44da25b11b04b9f1334ee78563a2ff11c6c
Sapphire Becker <github@logicplace.com>
2022-08-17 13:52:22 -0700

    fix I think? it's old

--- srec_cat.bat
+++ srec_cat.bat
@@ -1,5 +1,6 @@
-if exist "%PRODDIR%\..\bin-windows\srec_cat.exe" (
-	"%PRODDIR%\..\bin-windows\srec_cat.exe" %*
+@ECHO off
+if exist "%~dp0\bin-windows\srec_cat.exe" (
+	"%~dp0\bin-windows\srec_cat.exe" %*
 ) else (
 	start /D "%CD%" /unix /bin/sh -c "srec_cat %* >&2"
 )
