284cb68120806f2a775a13f9821a78889ef7ed75
Sapphire Becker <github@logicplace.com>
2022-08-19 03:22:26 -0700

    GBE+ installer

---
+++ installers/7zip.ps1
@@ -0,0 +1,50 @@
+<#
+.Description
+For extracting .7z archives
+#>
+param(
+[switch] $uninstall,
+[string] $prefix
+)
+
+if ([Environment]::Is64BitOperatingSystem) {
+	$DOWNLOAD_URL = 'https://www.7-zip.org/a/7z2201-x64.exe'
+} else {
+	$DOWNLOAD_URL = 'https://www.7-zip.org/a/7z2201.exe'
+}
+
+if ($prefix) {
+	if (! $sevenzip) {
+		Write-Host "7zip not installed"
+		exit 1
+	}
+
+	Split-Path $sevenzip
+} elseif ($uninstall) {
+	if ($sevenzip) {
+		if (Get-Command choco) {
+			choco uninstall 7zip
+		} else {
+			$dir = Split-Path $sevenzip
+			Write-Host 'Select Yes then click Uninstall'
+			& "$dir\Uninstall.exe"
+		}
+		Remove-Config 'sevenzip'
+	}
+} elseif (! $sevenzip) {
+	if ($loc = Get-Command 7z) {
+		Add-Config 'sevenzip' $loc[0].Source
+	} elseif (Get-Command choco) {
+		Start-Process choco -ArgumentList "install 7zip" -Verb runAs
+		Add-Config 'sevenzip' "$env:ChocolateyInstall\bin\7z.exe"
+	} else {
+		Download $DOWNLOAD_URL '7zip.exe'
+		Write-Host 'Make sure to copy the destination folder to your clipboard then click Install'
+		.\7zip.exe
+		$sevenzip = Read-Host 'Where did you install it?'
+		$sevenzip += '\7z.exe'
+		Add-Config 'sevenzip' $sevenzip
+	}
+}
+
+return $true
