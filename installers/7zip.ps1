389f84c517c7a6e5e778697144f71138db8702c3
Sapphire Becker <github@logicplace.com>
2025-03-17 01:09:09 -0700

    add winget support

--- installers/7zip.ps1
+++ installers/7zip.ps1
@@ -24,6 +24,8 @@ if ($prefix) {
 	if ($sevenzip) {
 		if (Get-Command choco) {
 			choco uninstall 7zip
+		} elseif (Get-Command winget) {
+			winget uninstall 7zip.7zip
 		} else {
 			$dir = Split-Path $sevenzip
 			Write-Host 'Select Yes then click Uninstall'
@@ -37,11 +39,14 @@ if ($prefix) {
 	} elseif (Get-Command choco) {
 		Start-Process choco -ArgumentList "install 7zip" -Verb runAs
 		Add-Config 'sevenzip' "$env:ChocolateyInstall\bin\7z.exe"
+	} elseif (Get-Command winget) {
+		winget install 7zip.7zip
+		Add-Config 'sevenzip' "C:\Program Files\7-Zip\7z.exe"
 	} else {
 		Download $DOWNLOAD_URL '7zip.exe'
 		Write-Host 'Make sure to copy the destination folder to your clipboard then click Install'
 		.\7zip.exe
-		$sevenzip = Read-Host 'Where did you install it?'
+		$sevenzip = Read-Host 'Where did you install it? (right-click to paste)'
 		$sevenzip += '\7z.exe'
 		Add-Config 'sevenzip' $sevenzip
 	}
