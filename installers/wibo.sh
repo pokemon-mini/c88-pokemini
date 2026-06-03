abb4baa979ea7a214311f87da72556253d947e68
Brad D thearchivalone <undergo-santa-malt@duck.com>
2026-06-03 12:24:40 -0500

    wibo install script init

---
+++ installers/wibo.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+source config.sh
+source installers/inc/common.sh
+
+# Wibo's standalone binary gets downloaded from here if Wine's not available
+wibo_url="https://github.com/decompals/wibo/releases/latest/download"
+
+# Standard info queries
+# Prefix isn't used by Wibo, so no need to call or set it
+uninstall=
+case "$1" in
+	--description)
+		echo "standalone wrapper for running 32-bit Windows apps on Linux and Mac; doesn't have as many features as wine but is portable, adds modern Mac support, and runs S1C88 tools"
+		exit 0
+		;;
+	--uninstall)
+		if "./wibo"; then
+			rm -r wibo > /dev/null 2>&1
+			echo "wibo removed"
+		fi
+		exit 0
+		;;
+	--*)
+		echo "Unknown command $1"
+		exit 2
+		;;
+esac
+
+arch=""
+if [[ "$(uname)" == "Linux" ]]; then
+	if [[ "$(uname -m)" == "x86_64" ]] || [[ "$(uname -m)" == "amd64" ]]; then
+		arch="x86_64"
+	elif [[ "$(uname -m)" == "x86" ]] || [[ "$(uname -m)" == "i686" ]]; then
+		arch="i686"
+	else
+		echo "Current Linux architecture not officially supported; install wine or build wibo from source and manually copy wibo binary to this directory"
+		exit 1
+	fi
+elif [[ "$(uname)" == "Darwin" ]]; then
+	if [[ "$(uname -m)" == "powerpc" ]]; then
+		echo "PowerPC Macs are not officially supported; install wine or build wibo from source and manually copy wibo binary to this directory"
+		exit 1
+	else
+		arch="macos"
+	fi
+else
+	echo "Platform not officially supported; install wine or build wibo from source and manually copy wibo binary to this directory"
+	exit 1
+fi
+
+rm -r wibo > /dev/null 2>&1
+curl -Lo wibo $wibo_url/wibo-$arch
+chmod 0755 wibo
+echo "wibo installed successfully"
--- installers/wine.sh
+++ installers/wine.sh
@@ -1,91 +1,36 @@
+# Bash on Linux and Mac both read scripts differently, so splitting them up for simplicity and sanity; parts will be duplicated but this is the most straightforward way to keep compatibility
 #!/bin/bash
-
 source config.sh
 source installers/inc/common.sh
 
-# Standard info queries
-uninstall=
-case "$1" in
-	--prefix)
-		if [ ! "$wine32" ]; then
-			echo "wine not installed"
-			exit 1
-		fi
-		echo "$(dirname "$wine32")"
-		exit 0
-		;;
-	--description)
-		echo "32-bit version of wine and wineboot for running Windows apps on Linux and Mac"
-		exit 0
-		;;
-	--uninstall)
-		if which apt; then
-			sudo apt purge wine wine-stable && sudo apt-get autoremove
-		elif which brew; then
-			brew uninstall wine-stable
-		else
-			echo "Can only uninstall wine with the package managers: apt, brew"
-			exit 1
-		fi
-		remove_config wine32
-		exit 0
-		;;
-	--*)
-		echo "Unknown command $1"
-		exit 2
-		;;
-esac
-
-# Check if it's already installed
-[ "$wine32" ] && exit 0  # From config
-
-purge=
-if which wine; then
-	case "$(file "$(which wine)")" in
-		*32-bit* | *i386*)
-			echo "wine32=\"$(which wine)\"" >> config.sh
-			exit 0
-			;;
-		*' ELF '*) purge=1; ;;
-		*'symbolic link'*)
-			grep -m1 wine32 "$(realpath "$(which wine)")" >> config.sh && exit 0
-			;&
-		*)
-			echo "Unknown wine install. If the 32-bit version of Wine is already installed, set wine32 to the full path config.sh"
-			exit 1
-			;;
-	esac
-fi
-
-if which apt; then
-	if [[ "$(uname -m)" == 'x86_64' ]]; then
-		for x in dpkg wget apt-add-repository; do
-			if ! which $x; then
-				echo "Requires system utility to install: $x"
-				exit 1
-			fi
-		done
-
-		[ "$purge" ] && sudo apt purge wine
-		sudo dpkg --add-architecture i386
-		wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
-		sudo apt-add-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -sc) main"
-		sudo apt update
-		sudo apt install --install-recommends winehq-stable
-	else
-		sudo apt install wine
+use_wine=0
+if [[ "$(uname)" == "Linux" ]]; then
+	./installers/wine-linux.sh
+	use_wine=1
+elif [[ "$(uname)" == "Darwin" ]]; then
+	# Get version to determine which path to take; Wibo has only been tested on modern Mac OS versions
+	version="$(sw_vers -productVersion)"
+	major=$(echo "$version" | awk -F'.' '{print $1}')
+	minor=$(echo "$version" | awk -F'.' '{print $2}')
+	# Wine won't work on macOS Catalina 10.15 or newer
+	if [[ "$major" -lt 10 ]]; then
+		./installers/wine-mac.sh
+		use_wine=1
+	elif [[ "$major" -eq 10 ]] && [[ "$minor" -lt 15 ]]; then
+		./installers/wine-mac.sh
+		use_wine=1
 	fi
-elif which brew; then
-	# Won't work on macOS Catalina 10.15 or newer)
-	brew tap homebrew/cask-versions
-	brew install --cask --no-quarantine wine-stable
-else
-	echo "Can only install wine with the package managers: apt, brew"
-	exit 1
 fi
 
-pfx="$(dirname "$(pwd)")/wineprefix"
-mkdir "$pfx"
-echo "#!/bin/sh" > wine
-echo "WINEARCH=win32 WINEPREFIX=$pfx wineboot" >> wine
-chmod 755 wine
+if [[ "$use_wine" == 1 ]]; then
+	pfx="$(dirname "$(pwd)")/wineprefix"
+	mkdir "$pfx"
+	echo "#!/bin/sh" > wine
+	echo "WINEARCH=win32 WINEPREFIX=$pfx wineboot" >> wine
+	chmod 755 wine
+else
+	echo "Could not install dependency: wine; attempting to install wibo"
+	if ! ./installers/wibo.sh; then
+		exit 1
+	fi
+fi
